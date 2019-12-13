44e21207e27d5a45458386ec8ef4c883b7ed5356
Artur112 <artur.jurgenson@hotmail.com>
2019-12-13 13:29:39 +0000

    Wrote code to obtain dice scores for the whole tumor, enhancing and necrotic core regions. Results are saved in an excel file.

--- segmentation_evaluation.py
+++ segmentation_evaluation.py
@@ -1,60 +1,135 @@
 import SimpleITK as sitk
 import numpy as np
 import matplotlib.pyplot as plt
-from data_loaders_old import Dataset
+from data_loaders import Dataset
 from models import Modified3DUNet
 import os
 import torch
-from enum import Enum
+import random
+from sklearn.model_selection import KFold
+from collections import OrderedDict
+import pandas as pd
+from pandas.plotting import table
+
+
+##################################################################################################################################
+# Code for evaluating the segmentation performance of a model. Returns Dice scores for the whole tumor, the enhancing and the
+# necrotic core regions for all the folds as well as the average over the folds. Saves the results in an excel sheet.
+
+# To specify:
+preprocessed_data_path = r'/home/artur-cmic/Desktop/UCL/Brats2019/Data/Preprocessed'
+model_saves_path = 'pretrained_models'  # Folder where the model saves should be stored in the format Fold_<fold_nr>_Epoch_<epoch_nr>.tar
+epoch_nr = 20  # Epoch at which to take the model saves (determined from loss plots)
+parallel_training = 1  # Specify if the model was trained with multiple GPUs (slightly more code needed to load model save)
+##################################################################################################################################
+
 
 # Get paths and names (IDS) of folders that store the multimodal training data
-data_path = r'C:\Users\artur\Desktop\UCL\Brats2019\Data\MICCAI_BraTS_2019_Data_Training\data'
 folder_paths = []
-folder_IDS = []
-for subdir in os.listdir(data_path):
-    folder_paths.append(os.path.join(data_path,subdir))
-    folder_IDS.append(subdir)
+folder_ids = []
+for subdir in os.listdir(preprocessed_data_path):
+    folder_paths.append(os.path.join(preprocessed_data_path, subdir))
+    folder_ids.append(subdir)
 
-train_set = Dataset(folder_paths, folder_IDS)
+# Shuffle them around, keeping same seed to make sure same shuffling is used if training is interrupted and needs to be continued
+random.seed(4)
+random.shuffle(folder_paths)
+random.seed(4)
+random.shuffle(folder_ids)
 
-model = Modified3DUNet(4, 4, 16)
-#checkpoint = torch.load("../KFold_Cross_Validation/Fold_1_Epoch_125.tar")
-model.load_state_dict(torch.load("pretrained_models/Modified3DUNet_Epoch_200.pt"))
-#model.load_state_dict(checkpoint['model_state_dict'])
-model.eval()
+# Use GPU
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda:0" if use_cuda else "cpu")
+torch.backends.cudnn.benchmark = True
 
-for idx in [12,55,142,22]:
-    scans, labels = train_set[idx]
-    scans = scans.unsqueeze(0)
-    output, seg_layer = model(scans)
-    seg_layer = seg_layer.squeeze()
-    _, indices = seg_layer.max(0)
-    labels = labels.numpy()
-    #print(np.unique(labels))
-    indices = indices.numpy()
-    #print(np.unique(indices))
-    overlap_measures_filter = sitk.LabelOverlapMeasuresImageFilter()
-    overlap_measures_filter.Execute(sitk.GetImageFromArray(indices), sitk.GetImageFromArray(labels))
-    dice = overlap_measures_filter.GetDiceCoefficient()
-    plt.figure()
-    plt.subplot(1,2,1)
-    plt.imshow(indices[:,:,50])
-    plt.title('Model output')
-    plt.subplot(1,2,2)
-    plt.imshow(labels[:,:,50])
-    plt.title('Ground truth')
-    plt.suptitle("Dice score = " + str(dice))
-    plt.show()
+# Setup KFold Cross Validation
+n_folds = 5  # Number of folds in cross-validation
+kf = KFold(n_splits=n_folds, shuffle=False)  # Shuffle=false to get the same shuffling scheme every run
 
-    measures = {}
-    measures['dice'] = overlap_measures_filter.GetDiceCoefficient()
-    measures['jaccard'] = overlap_measures_filter.GetJaccardCoefficient()
-    measures['volume similarity'] = overlap_measures_filter.GetVolumeSimilarity()
-    measures['false negatives'] = overlap_measures_filter.GetFalseNegativeError()
-    measures['false positives'] = overlap_measures_filter.GetFalsePositiveError()
+results = pd.DataFrame()
+fold_nr = 1
+for fold in kf.split(folder_paths):
+    print("Getting Model Performance on Fold {}/{}".format(fold_nr, n_folds))
+    valid_idx = fold[1]
+    valid_set = Dataset([folder_paths[i] for i in valid_idx], [folder_ids[i] for i in valid_idx])
+    model = Modified3DUNet(4, 4, 16)
+    checkpoint = torch.load("{}/Fold_{}_Epoch_{}.tar".format(model_saves_path,fold_nr, epoch_nr))
 
-    for key, item in measures.items():
-        print(key, "\t", item)
+    if(parallel_training): # If training was done on multiple GPUs have to rename keys in saved model_state_dict:
+        new_state_dict = OrderedDict()
+        for k, v in checkpoint['model_state_dict'].items():
+            name = k[7:]  # remove module.
+            new_state_dict[name] = v
+        model.load_state_dict(new_state_dict)
+    else:
+        model.load_state_dict(checkpoint['model_state_dict'])
+    model.to(device)
+    model.eval()
 
+    fold_measures = {}
+    for idx in range(0, len(valid_idx)): # Loop over all the data examples
+        with torch.no_grad():
+            scans, labels = valid_set[idx]
+            scans = np.expand_dims(scans,0)
+            scans = torch.from_numpy(scans).to(device)
+            output, seg_layer = model(scans)
+            seg_layer = seg_layer.squeeze()
+            _, indices = seg_layer.max(0)
+            indices = indices.cpu().detach().numpy()
+
+            whole_tumor_pred = ((indices == 3) | (indices == 1)).astype('int')
+            enhancing_pred = (indices == 3).astype('int')
+            core_pred = (indices == 1).astype('int')
+
+            whole_tumor_gt = ((labels == 3) | (labels == 1)).astype('int')
+            enhancing_gt = (labels == 3).astype('int')
+            core_gt = (labels == 1).astype('int')
+
+            comparisons = {'Whole': {'pred': whole_tumor_pred, 'gt': whole_tumor_gt}, 'Enhancing': {'pred': enhancing_pred, 'gt': enhancing_gt},
+                           'Core': {'pred': core_pred, 'gt': core_gt}}
+            measures = {}
+            for region_name, masks in comparisons.items():
+                overlap_measures_filter = sitk.LabelOverlapMeasuresImageFilter()
+                overlap_measures_filter.Execute(sitk.GetImageFromArray(masks['pred']), sitk.GetImageFromArray(masks['gt']))
+
+                #dice = overlap_measures_filter.GetDiceCoefficient()
+                #plt.figure()
+                #plt.subplot(1,2,1)
+                #plt.imshow(indices[:,:,50])
+                #plt.title('Model output')
+                #plt.subplot(1,2,2)
+                #plt.imshow(labels[:,:,50])
+                #plt.title('Ground truth')
+                #plt.suptitle("Dice score = " + str(dice))
+                #plt.show()
+
+                measures['{}, Dice'.format(region_name)] = overlap_measures_filter.GetDiceCoefficient()
+                #measures['{}, Jaccard'.format(region_name)] = overlap_measures_filter.GetJaccardCoefficient()
+            fold_measures[idx] = measures
+
+    fold_measures = pd.DataFrame.from_dict(fold_measures, orient='index').astype('float')
+    fold_measures = fold_measures.replace([np.inf, -np.inf], np.nan)
+    fold_mean = fold_measures.mean(axis=0, skipna=True)
+    fold_std = fold_measures.std(axis=0, skipna=True)
+    mean_keys = [x + ', Mean' for x in fold_measures.keys()]
+    std_keys = [x + ', Std' for x in fold_measures.keys()]
+    if(fold_nr == 1):
+        results = pd.DataFrame(columns = mean_keys + std_keys)
+        results.loc['Fold {}'.format(fold_nr)] = list(fold_mean) + list(fold_std)
+    else:
+        results.loc['Fold {}'.format(fold_nr)] = list(fold_mean) + list(fold_std)
+    fold_nr += 1
+
+results.loc['Overall'] = results.mean(axis=0)
+results = results.astype('float').round(3)
+a = results.columns.str.split(', ', expand=True).values
+results.columns = pd.MultiIndex.from_tuples([x for x in a])
+results = results.sort_index(1)
+writer = pd.ExcelWriter('overlap_results.xlsx', engine='xlsxwriter')
+results.to_excel(writer)
+writer.save()
+
+print("Segmentation Performance: ")
+print(results)
 
 
