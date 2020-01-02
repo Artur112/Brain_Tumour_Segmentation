1449dbda293e852d3622317f3aa89a4966276dcc
Artur112 <artur.jurgenson@hotmail.com>
2020-01-02 01:42:14 +0000

    Added Random Noise addition to Data Augmentation

--- data_augment.py
+++ data_augment.py
@@ -9,10 +9,13 @@ import elasticdeform
 ###################################################################################
 
 class DataAugment():
-    def __init__(self, x, y):
+    def __init__(self, x, y, augmentations_to_use=None):
+        if augmentations_to_use is None:
+            augmentations_to_use = ['Elastic', 'Rotate', 'Flip', 'Gamma', 'Noise', 'Scale']
         self.scans = x
         self.mask = y
         self.batch_size = x.shape[0]
+        self.augmentations = augmentations_to_use
 
     def elastic_deform(self,X,Y):
         X = X.numpy()
@@ -61,15 +64,18 @@ class DataAugment():
     def gamma_correction(self,X):
         gamma = random.uniform(0.5, 1.5)  # Random gamma value by which to correct
         X = torch.pow(X, gamma)
+        return X
 
+    def random_noise(self, X):
+        X = X + (0.1**0.5)*torch.randn(X.shape)
         return X
 
     def scale(self,X,Y):
         scale_nr = random.sample([0.50, 0.75, 1, 1.25, 1.5], k=1)[0]  # Random scale by which to change image size
         in_dim = X.shape[2]
         out_dim = int(in_dim * scale_nr) # New size to scale to
+        Y = torch.unsqueeze(Y,0)
         # Scale labels separately again
-        Y = torch.unsqueeze(Y, 0)
         lbl1 = (Y == 1) * 10.0
         lbl2 = (Y == 2) * 10.0
         lbl3 = (Y == 3) * 10.0
@@ -77,48 +83,52 @@ class DataAugment():
         lbl1 = torch.nn.functional.interpolate(lbl1, (out_dim, out_dim, out_dim), mode='trilinear')
         lbl2 = torch.nn.functional.interpolate(lbl2, (out_dim, out_dim, out_dim), mode='trilinear')
         lbl3 = torch.nn.functional.interpolate(lbl3, (out_dim, out_dim, out_dim), mode='trilinear')
-        lbl1 = lbl1.squeeze(0)
-        lbl2 = lbl2.squeeze(0)
-        lbl3 = lbl3.squeeze(0)
         lbl1[lbl1 < 3] = -1
         lbl2[lbl2 < 3] = -1
         lbl3[lbl3 < 3] = -1
-        _, Y = torch.stack([torch.zeros(self.batch_size,out_dim, out_dim, out_dim), lbl1, lbl2, lbl3],dim=0).max(0)
+        _, Y = torch.stack([torch.zeros(1, self.batch_size, out_dim, out_dim, out_dim), lbl1, lbl2, lbl3], dim=0).max(0)
         Y = Y.long()
         return X, Y
 
     def augment(self):
 
+        to_scale = False
         # Except for scaling, perform the data augmentations separately for every person in the batch
         for person in range(self.batch_size):
             x = self.scans[person]
             y = self.mask[person]
             y = torch.unsqueeze(y,0)  # As x is in 4D, make y 4D too so the scans and the labels can be inputted together to the various functions
 
-
-            # Random elastic deformation
-            if(random.random() > 0.5):
-                x, y = self.elastic_deform(x, y)
-
-            # Random rotation
-            if(random.random() > 0.5):
-                x,y = self.rotate(x,y)
-
-            # Random flip
-            if(random.random() > 0.5):
-                x,y = self.flip(x,y)
-
-            # Random gamma correction
-            if(random.random() > 0.5):
-                x = self.gamma_correction(x)
+            for augmentation in self.augmentations:
+                if(augmentation == 'Elastic'):
+                    # Random elastic deformation
+                    if(random.random() > 0.5):
+                        x, y = self.elastic_deform(x, y)
+                elif(augmentation == 'Rotate'):
+                    # Random rotation
+                    if(random.random() > 0.5):
+                        x,y = self.rotate(x,y)
+                elif(augmentation == 'Flip'):
+                    # Horizontal flip
+                    if(random.random() > 0.5):
+                        x,y = self.flip(x,y)
+                elif(augmentation == 'Gamma'):
+                    # Random gamma correction
+                    if(random.random() > 0.5):
+                        x = self.gamma_correction(x)
+                elif(augmentation == 'Noise'):
+                    if (random.random() > 0.5):
+                        x = self.random_noise(x)
+                elif(augmentation == 'Scale'):
+                    to_scale = True
 
             y = torch.squeeze(y, 0)
-
             self.scans[person] = x
             self.mask[person] = y
 
         # Random scaling
-        if (random.random() > 0.5):
-            self.scans, self.mask = self.scale(self.scans, self.mask)
+        if to_scale:
+            if (random.random() > 0.5):
+                self.scans, self.mask = self.scale(self.scans, self.mask)
 
         return self.scans, self.mask
