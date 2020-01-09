5355ebf3ee7b8b5f4642aae8fc57dbe37c4532f5
Artur112 <artur.jurgenson@hotmail.com>
2020-01-09 11:58:25 +0000

    Made codes callable as scripts

--- evaluate.py
+++ evaluate.py
@@ -1,138 +1,88 @@
 import SimpleITK as sitk
-import numpy as np
 import nibabel as nib
-import matplotlib.pyplot as plt
-from data_loaders import Dataset
-from models import Modified3DUNet
 import os
-import torch
-import random
-from sklearn.model_selection import KFold
-from collections import OrderedDict
 import pandas as pd
+from sys import argv
 
 
-##################################################################################################################################
-# Code for evaluating the segmentation performance of a model. Returns Dice scores for the whole tumor, the enhancing and the
-# necrotic core regions for all the folds as well as the average over the folds. Saves the results in an excel sheet.
+########################################################################################################################
+# Code for evaluating the segmentation performance of a model. Returns Dice scores for the whole tumor, the enhancing
+# and the necrotic core regions for all the folds as well as the average over the folds. Saves the results in an excel
+# sheet. When calling script:
 
-# To specify:
-# preprocessed_data_path = r'/home/artur-cmic/Desktop/UCL/Brats2019/Data/Preprocessed'
-preprocessed_data_path = r'C:/users/artur/Desktop/UCL/Brats2019/Data/Preprocessed'
-model_saves_path = r'C:/users/artur/Desktop/UCL/Brats2019/Model_Saves/V3_Prepro_Aug_CrossEn'  # Folder where the model saves should be stored in the format Fold_<fold_nr>_Epoch_<epoch_nr>.tar
-epoch_nr = 180  # Epoch at which to take the model saves (determined from loss plots)
-parallel_training = 0  # Specify if the model was trained with multiple GPUs (slightly more code needed to load model save)
-experiment_name = 'V3'
-##################################################################################################################################
+# INPUT Arguments:
+#   arg1: path to where the segmented scans are stored
+#   arg2: path to where the original brats folders for those scans are stored
+#   arg3: path where to store excel sheet of results for current run
+#   arg4: nothing or 1: whether to add mean dice score to All_Results.xlsx file if one wants to store the results of all the
+#         runs in a same file.
 
+# OUTPUT:
+#   Excel sheet of segmentation scores stored in the provided save results path
+########################################################################################################################
+segmented_data_path = argv[1]
+original_data_path = argv[2]
+save_results_path = argv[3]
+all_runs_path = ""
+if len(argv) > 4:
+    all_runs_path = argv[4]
+run_name = segmented_data_path[segmented_data_path.rindex("/") + 1:]
+print("Getting Results for run " + run_name)
 
-# Get paths and names (IDS) of folders that store the multimodal training data
-folder_paths = []
-folder_ids = []
-for subdir in os.listdir(preprocessed_data_path):
-    folder_paths.append(os.path.join(preprocessed_data_path, subdir))
-    folder_ids.append(subdir)
+# Get the paths of the files in the segmented scans folder
+seg_paths = []
+seg_ids = []
+for file_to_segment in os.listdir(segmented_data_path):
+    seg_paths.append(os.path.join(segmented_data_path, file_to_segment))
+    seg_ids.append(file_to_segment[:-7])
 
-# Shuffle them around, keeping same seed to make sure same shuffling is used if training is interrupted and needs to be continued
-random.seed(4)
-random.shuffle(folder_paths)
-random.seed(4)
-random.shuffle(folder_ids)
+results = {}
+for image in range(len(seg_paths)):
+    ground_truth = nib.load(os.path.join(original_data_path, seg_ids[image],seg_ids[image])+"_seg.nii.gz").get_data().astype('uint8')
+    prediction = nib.load(seg_paths[image]).get_data().astype('uint8')
 
-# Use GPU
-use_cuda = torch.cuda.is_available()
-device = torch.device("cuda:0" if use_cuda else "cpu")
-torch.backends.cudnn.benchmark = True
+    whole_tumor_gt = (ground_truth > 0).astype('int')
+    enhancing_gt = (ground_truth == 4).astype('int')
+    core_gt = (ground_truth == 1).astype('int')
 
-# Setup KFold Cross Validation
-n_folds = 5  # Number of folds in cross-validation
-kf = KFold(n_splits=n_folds, shuffle=False)  # Shuffle=false to get the same shuffling scheme every run
+    whole_tumor_pred = (prediction > 0).astype('int')
+    enhancing_pred = (prediction == 4).astype('int')
+    core_pred = (prediction == 1).astype('int')
 
-results = pd.DataFrame()
-fold_nr = 1
-for fold in kf.split(folder_paths):
-    if(fold_nr ==1):
-        print("Getting Model Performance on Fold {}/{}".format(fold_nr, n_folds))
-        valid_idx = fold[1]
-        valid_set = Dataset([folder_paths[i] for i in valid_idx], [folder_ids[i] for i in valid_idx])
-        model = Modified3DUNet(4, 4, 16)
-        checkpoint = torch.load("{}/Fold_{}_Epoch_{}.tar".format(model_saves_path,fold_nr, epoch_nr))
+    comparisons = {'Whole': {'pred': whole_tumor_pred, 'gt': whole_tumor_gt}, 'Enhancing': {'pred': enhancing_pred, 'gt': enhancing_gt},
+                   'Core': {'pred': core_pred, 'gt': core_gt}}
+    measures = {}
+    for region_name, masks in comparisons.items():
+        overlap_measures_filter = sitk.LabelOverlapMeasuresImageFilter()
+        overlap_measures_filter.Execute(sitk.GetImageFromArray(masks['pred']), sitk.GetImageFromArray(masks['gt']))
+        measures['{}, Dice'.format(region_name)] = overlap_measures_filter.GetDiceCoefficient()
+        #measures['{}, Jaccard'.format(region_name)] = overlap_measures_filter.GetJaccardCoefficient()
+    results[seg_ids[image]] = measures
 
-        if(parallel_training): # If training was done on multiple GPUs have to rename keys in saved model_state_dict:
-            new_state_dict = OrderedDict()
-            for k, v in checkpoint['model_state_dict'].items():
-                name = k[7:]  # remove module.
-                new_state_dict[name] = v
-            model.load_state_dict(new_state_dict)
-        else:
-            model.load_state_dict(checkpoint['model_state_dict'])
-        model.to(device)
-        model.eval()
-
-        fold_measures = {}
-        for idx in range(0, len(valid_idx)): # Loop over all the data examples
-            with torch.no_grad():
-                scans, labels = valid_set[idx]
-                scans = np.expand_dims(scans,0)
-                scans = torch.from_numpy(scans).to(device)
-                output, seg_layer = model(scans)
-                seg_layer = seg_layer.squeeze()
-                _, indices = seg_layer.max(0)
-                indices = indices.cpu().detach().numpy()
-
-                whole_tumor_pred = ((indices == 3) | (indices == 1)).astype('int')
-                enhancing_pred = (indices == 3).astype('int')
-                core_pred = (indices == 1).astype('int')
-
-                whole_tumor_gt = ((labels == 3) | (labels == 1)).astype('int')
-                enhancing_gt = (labels == 3).astype('int')
-                core_gt = (labels == 1).astype('int')
-
-                comparisons = {'Whole': {'pred': whole_tumor_pred, 'gt': whole_tumor_gt}, 'Enhancing': {'pred': enhancing_pred, 'gt': enhancing_gt},
-                               'Core': {'pred': core_pred, 'gt': core_gt}}
-                measures = {}
-                for region_name, masks in comparisons.items():
-                    overlap_measures_filter = sitk.LabelOverlapMeasuresImageFilter()
-                    overlap_measures_filter.Execute(sitk.GetImageFromArray(masks['pred']), sitk.GetImageFromArray(masks['gt']))
-
-                    #dice = overlap_measures_filter.GetDiceCoefficient()
-                    #plt.figure()
-                    #plt.subplot(1,2,1)
-                    #plt.imshow(indices[:,:,50])
-                    #plt.title('Model output')
-                    #plt.subplot(1,2,2)
-                    #plt.imshow(labels[:,:,50])
-                    #plt.title('Ground truth')
-                    #plt.suptitle("Dice score = " + str(dice))
-                    #plt.show()
-
-                    measures['{}, Dice'.format(region_name)] = overlap_measures_filter.GetDiceCoefficient()
-                    #measures['{}, Jaccard'.format(region_name)] = overlap_measures_filter.GetJaccardCoefficient()
-                fold_measures[idx] = measures
-
-        fold_measures = pd.DataFrame.from_dict(fold_measures, orient='index').astype('float')
-        fold_measures = fold_measures.replace([np.inf, -np.inf], np.nan)
-        fold_mean = fold_measures.mean(axis=0, skipna=True)
-        fold_std = fold_measures.std(axis=0, skipna=True)
-        mean_keys = [x + ', Mean' for x in fold_measures.keys()]
-        std_keys = [x + ', Std' for x in fold_measures.keys()]
-        if(fold_nr == 1):
-            results = pd.DataFrame(columns = mean_keys + std_keys)
-            results.loc['Fold {}'.format(fold_nr)] = list(fold_mean) + list(fold_std)
-        else:
-            results.loc['Fold {}'.format(fold_nr)] = list(fold_mean) + list(fold_std)
-        fold_nr += 1
-
-results.loc['Overall'] = results.mean(axis=0)
+results = pd.DataFrame.from_dict(results, orient='index').astype('float')
+res_mean = results.mean(axis=0, skipna=True)
+res_std = results.std(axis=0, skipna=True)
+results.loc['Overall Mean'] = res_mean
+results.loc['Overall Std'] = res_std
 results = results.astype('float').round(3)
-a = results.columns.str.split(', ', expand=True).values
-results.columns = pd.MultiIndex.from_tuples([x for x in a])
-results = results.sort_index(1)
-writer = pd.ExcelWriter('overlap_results_{}.xlsx'.format(experiment_name), engine='xlsxwriter')
+writer = pd.ExcelWriter('{}/overlap_{}.xlsx'.format(save_results_path,run_name), engine='xlsxwriter')
 results.to_excel(writer)
 writer.save()
 
-print("Segmentation Performance: ")
-print(results)
-
+if all_runs_path:
+    if not os.path.isfile(os.path.join(save_results_path, "All_Results.xlsx")):
+        res_mean['Run'] = run_name
+        all_results = pd.DataFrame([res_mean])
+        all_results.set_index('Run', inplace=True, drop=True)
+        all_results = all_results.round(3)
+        writer = pd.ExcelWriter('{}/All_Results.xlsx'.format(save_results_path), engine='xlsxwriter')
+        all_results.to_excel(writer)
+        writer.save()
+    else:
+        all_results = pd.read_excel('{}/All_Results.xlsx'.format(save_results_path), index_col=0)
+        all_results.loc[run_name] = res_mean
+        all_results = all_results.round(3)
+        writer = pd.ExcelWriter('{}/All_Results.xlsx'.format(save_results_path), engine='xlsxwriter')
+        all_results.to_excel(writer)
+        writer.save()
 
--- segment.py
+++ segment.py
@@ -1,21 +1,33 @@
 import numpy as np
 import nibabel as nib
 from data_loaders import Dataset
-from models import Modified3DUNet
+from models import UNet3D
 from skimage.transform import resize
 import os
 import torch
 from collections import OrderedDict
+from sys import argv
 
-#preprocessed_valid_data_path = r'C:/users/artur/Desktop/UCL/Brats2019/Data/Preprocessed_Validation'
-preprocessed_valid_data_path = r'/home/artur-cmic/Desktop/UCL/Brats2019/Data/Preprocessed_Validation'
-#model_saves_path = r'C:/users/artur/Desktop/UCL/Brats2019/Model_Saves/V4_Prepro_NoAug_Dice'  # Folder where the model saves should be stored in the format Fold_<fold_nr>_Epoch_<epoch_nr>.tar
-model_saves_path = r'/home/artur-cmic/Desktop/UCL/Brats2019/Model_Saves/V4_Prepro_NoAug_Dice'
-epoch_nr = 250  # Epoch at which to take the model saves (determined from loss plots)
-parallel_training = 1  # Specify if the model was trained with multiple GPUs (slightly more code needed to load model save)
-#save_results_path = r'C:/users/artur/Desktop/UCL/Brats2019/Seg_Results_V4'
-save_results_path = r'/home/artur-cmic/Desktop/UCL/Brats2019/Seg_Results/Seg_Results_V4'
-folds = [1, 2, 3, 4, 5]  # Fold across which to ensemble average output
+########################################################################################################################
+# Code to segment a set of scans after model has been trained. Segmentation obtained by ensemble averaging outputs of the
+# models from the different folds. When calling script:
+
+# INPUT arguments:
+#   arg1: path to where the preprocessed scans to segment are stored
+#   arg2: path to where the Model_Saves are stored
+#   arg3: epoch nr at which to to load a models save
+#   arg4: path where to save the segmented scans. Code creates folder if it doesnt exist
+
+# OUTPUT:
+#   segmented scans stored as nii.gz files in provided save results path
+########################################################################################################################
+
+preprocessed_data_path = argv[1]
+model_saves_path = argv[2]
+epoch_nr = int(argv[3])  # Epoch at which to take the model saves (determined from loss plots)
+save_results_path = argv[4]
+
+run_name = model_saves_path[model_saves_path.rindex("/") + 1:]
 
 if not os.path.isdir(save_results_path):
     os.mkdir(save_results_path)
@@ -23,8 +35,8 @@ if not os.path.isdir(save_results_path):
 # Get paths and names (IDS) of folders that store the multimodal training data
 folder_paths = []
 folder_ids = []
-for subdir in os.listdir(preprocessed_valid_data_path):
-    folder_paths.append(os.path.join(preprocessed_valid_data_path, subdir))
+for subdir in os.listdir(preprocessed_data_path):
+    folder_paths.append(os.path.join(preprocessed_data_path, subdir))
     folder_ids.append(subdir)
 
 valid_set = Dataset(folder_paths, folder_ids, False)
@@ -35,10 +47,10 @@ device = torch.device("cuda:0" if use_cuda else "cpu")
 torch.backends.cudnn.benchmark = True
 
 models = []
-for fold in folds:
-    model = Modified3DUNet(4, 4, 16)
+for fold in range(1,6):
+    model = UNet3D(4, 4, False, 16, 'crg', 8)
     checkpoint = torch.load("{}/Fold_{}_Epoch_{}.tar".format(model_saves_path, fold, epoch_nr))
-    if parallel_training:  # If training was done on multiple GPUs have to rename keys in saved model_state_dict:
+    if list(checkpoint['model_state_dict'].keys())[0].find("module") > -1:
         new_state_dict = OrderedDict()
         for k, v in checkpoint['model_state_dict'].items():
             name = k[7:]  # remove module.
@@ -56,10 +68,10 @@ for idx in range(0, len(folder_ids)):
         scans = np.expand_dims(scans, 0)
         scans = torch.from_numpy(scans).to(device)
         seg_results = []
-        for fold in folds:
-            output, seg_layer = models[fold-1](scans)
-            seg_layer = seg_layer.squeeze()
-            seg_results.append(seg_layer)
+        for fold in range(1,6):
+            output = models[fold-1](scans)
+            output = output.squeeze()
+            seg_results.append(output)
 
         seg_layer_avg = torch.mean(torch.stack(seg_results), dim=0)
         #print(seg_layer_avg.shape)
@@ -83,4 +95,4 @@ for idx in range(0, len(folder_ids)):
         img_segm[img_segm == 3] = 4
         img = nib.Nifti1Image(img_segm, [[-1,-0,-0,0],[-0,-1,-0,239],[0,0,1,0],[0,0,0,1]])
         nib.save(img, "{}/{}.nii.gz".format(save_results_path, folder_ids[idx]))
-        print('Saved example {}/{}'.format(idx + 1, len(folder_ids)))
+        print('Saved example {}/{} for run {}'.format(idx + 1, len(folder_ids), run_name))
